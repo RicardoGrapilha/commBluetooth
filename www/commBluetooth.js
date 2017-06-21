359675790cc973413eadcabbaf55de7223aece95
ricardo <ricardo@localhost.localdomain>
2017-06-21 12:34:54 -0300

    converter string v1.4

--- www/commBluetooth.js
+++ www/commBluetooth.js
@@ -38,8 +38,8 @@ module.exports = {
         console.log("message para java", message);
         cordova.exec(success, failure, "CommBluetooth", "SEND_MESSAGE", [message]);
     },
-    bufferToStr: function(buf){
-    	return String.fromCharCode.apply(null, new Uint16Array(buf));
+    arrayBufferToStr: function(arrayBuf){
+    	return decodeBase64(String.fromCharCode.apply(null, new Uint16Array(arrayBuf)));
     }
 };
 
@@ -49,4 +49,15 @@ var stringToArrayBuffer = function(str) {
         ret[i] = str.charCodeAt(i);
     }
     return ret.buffer;
+};
+
+var decodeBase64 = function(s) {
+    var e={},i,b=0,c,x,l=0,a,r='',w=String.fromCharCode,L=s.length;
+    var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    for(i=0;i<64;i++){e[A.charAt(i)]=i;}
+    for(x=0;x<L;x++){
+        c=e[s.charAt(x)];b=(b<<6)+c;l+=6;
+        while(l>=8){((a=(b>>>(l-=8))&0xff)||(x<(L-2)))&&(r+=w(a));}
+    }
+    return r;
 };
