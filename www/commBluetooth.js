93c9d4306df6a1e3ca9db14d1d03b52533d12c0d
ricardo <ricardo@localhost.localdomain>
2017-06-28 11:45:39 -0300

    editado o modo de tratar o plugin v1.2

--- www/commBluetooth.js
+++ www/commBluetooth.js
@@ -50,28 +50,28 @@ var exec = require('cordova/exec');
     };
     commBluetooth.disconnect = function (success,failure){
     	exec(success, failure, "CommBluetooth", "DISCONNECT", []);
-    ;
+    };
 
 
 
-var stringToArrayBuffer = function(str) {
-    var ret = new Uint8Array(str.length);
-    for (var i = 0; i < str.length; i++) {
-        ret[i] = str.charCodeAt(i);
-    }
-    return ret.buffer;
-};
+	var stringToArrayBuffer = function(str) {
+	    var ret = new Uint8Array(str.length);
+	    for (var i = 0; i < str.length; i++) {
+	        ret[i] = str.charCodeAt(i);
+	    }
+	    return ret.buffer;
+	};
+	
+	var decodeBase64 = function(s) {
+	    var e={},i,b=0,c,x,l=0,a,r='',w=String.fromCharCode,L=s.length;
+	    var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	    for(i=0;i<64;i++){e[A.charAt(i)]=i;}
+	    for(x=0;x<L;x++){
+	        c=e[s.charAt(x)];b=(b<<6)+c;l+=6;
+	        while(l>=8){((a=(b>>>(l-=8))&0xff)||(x<(L-2)))&&(r+=w(a));}
+	    }
+	    return r;
+	};
 
-var decodeBase64 = function(s) {
-    var e={},i,b=0,c,x,l=0,a,r='',w=String.fromCharCode,L=s.length;
-    var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    for(i=0;i<64;i++){e[A.charAt(i)]=i;}
-    for(x=0;x<L;x++){
-        c=e[s.charAt(x)];b=(b<<6)+c;l+=6;
-        while(l>=8){((a=(b>>>(l-=8))&0xff)||(x<(L-2)))&&(r+=w(a));}
-    }
-    return r;
-};
 
-
-module.exports = commBluetooth;
+	module.exports = commBluetooth;
